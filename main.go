4ff4cc776e1aeb0e26435a9068a04ac1a28ee72b
Sed0y <stdor@yandex.ru>
2018-10-10 14:07:16 +0300

    new

--- main.go
+++ main.go
@@ -1,67 +1,54 @@
 package main
 
 import (
-	//"cat/conf"
-	//"cat/controllers"
-	//"cat/models"
+	"cat/conf"
+	"cat/controllers"
+	"cat/models"
 
 	//_ "cat/routers"
-	//"database/sql"
+	"database/sql"
 
-	//"github.com/astaxie/beego"
+	"github.com/astaxie/beego"
 
 	"fmt"
-	//"log"
-	//_ "github.com/lib/pq"
+	"log"
+
+	_ "github.com/lib/pq"
 )
 
-//var App models.Application
+var App models.Application
 
 func init() {
 
-	/*
-		var err error
+	var err error
 
-		// Инициализация подключения в БД PostgreSQL
-		dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
-			conf.PostgresUser, conf.PostgresPassword, conf.PostgresDB)
+	// Инициализация подключения в БД PostgreSQL
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
+		conf.PostgresUser, conf.PostgresPassword, conf.PostgresDB)
 
-		conf.DB_postgres, err = sql.Open("postgres", dbinfo)
-		if err != nil {
-			log.Fatal(err)
-		} else {
-			fmt.Println("Database connected")
-			App.Categories.Load()
-			controllers.App = &App
-		}
-	*/
+	conf.DB_postgres, err = sql.Open("postgres", dbinfo)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("Database connected")
+		App.Categories.Load()
+		controllers.App = &App
+	}
 
 }
 
 func main() {
 
-	/*
-		var app app.Application
+	fmt.Println("start")
 
-		app.Categories.Load()
+	for i := 0; i < len(App.Categories.List); i++ {
+		fmt.Println(App.Categories.List[i])
+	}
 
-		app.Categories.Remove(2)
-		app.Categories.Remove(3)
+	fmt.Println("end")
 
-		app.Categories.Add(0, 1, 15, "Первая")
-		app.Categories.Add(0, 1, 16, "Вторая")
-		app.Categories.Add(0, 1, 16, "Третья")
+	return
 
-		fmt.Println("start")
-
-		for i := 0; i < len(app.Categories.List); i++ {
-			fmt.Println(app.Categories.List[i])
-		}
-
-		fmt.Println("end")
-
-		return
-	*/
 	fmt.Println("start!")
-	//beego.Run()
+	beego.Run()
 }
